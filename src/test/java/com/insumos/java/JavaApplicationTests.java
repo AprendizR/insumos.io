11eed81bf5cf2a994b60384a94fae53585a4c5d6
Renan Rosa <renanrosa1005@gmail.com>
2026-06-26 10:34:48 -0300

    commit inicial

---
+++ src/test/java/com/insumos/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insumos.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
